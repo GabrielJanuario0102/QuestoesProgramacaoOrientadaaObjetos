92af6974a25afc41dfae5761a3ef901596d992ac
GabrielJanuario0102 <gabriel.januario@academico.ifpb.edu.br>
2025-10-09 12:08:22 -0300

    CodigosObjetos
    Conta e Helicoptero

--- Conta.java
+++ Conta.java
@@ -3,6 +3,88 @@ package QuestoesPOO;
 public class Conta {
     private int numeroConta;
     private String nomeCliente;
-    private int saldo;
-    private 
+    private float saldo;
+    private Boolean ativa;
+
+    public Conta(String nomeCliente, int numeroConta) {
+        this.numeroConta = numeroConta;
+        this.nomeCliente = nomeCliente;
+        this.saldo = 0;
+        this.ativa = true;
+    }
+
+    public void setNumeroConta(int numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+    public void setNomeCLiente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+    public void setAtiva(Boolean ativa) {
+        this.ativa = ativa;
+    }
+
+
+    public int getNumeroConta() {
+        return this.numeroConta;
+    }
+    public String getNomeCliente() {
+        return this.nomeCliente;
+    }
+    public float getSaldo() {
+        return this.saldo;
+    }
+    public Boolean getAtiva() {
+        return this.ativa;
+    }
+
+
+
+
+    public void depositar(float valorDeposito) {
+        if (valorDeposito >= 0 && getAtiva() == false) {
+            System.out.println("Impossivel depositar, valor menor igual a 0, ou conta desativada.");
+        }else{
+            setSaldo(valorDeposito);
+            System.out.println("Foi depositado: " +valorDeposito+ " na Conta.");
+        }
+    }
+
+    public void sacar(float valorSaque) {
+        if (valorSaque <=0 || getAtiva() == false) {
+            System.out.println("Impossivel sacar, valor de saque baixo ou conta desativada.");
+        } else if (valorSaque > getSaldo()) {
+            System.out.println("Impossivel sacar, pois o valor de saque é maior que o valor em saldo.");
+        } else {
+            setSaldo(valorSaque);
+            System.out.println("Foi sacado: " +valorSaque+ " na Conta.");
+        }
+    } 
+
+    public void desativar() {
+        if (getAtiva() == false) {
+            System.out.println("Impossivel desativar, pois conta ja está desativada.");
+        }else {
+            setAtiva(false);
+            System.out.println("Conta foi desativada.");
+        }
+    }
+
+    public void ativar() {
+        if (getAtiva() == true) {
+            System.out.println("Impossivel ativar, conta ja está ativa.");
+        } else {
+            setAtiva(true);
+            System.out.println("Conta foi ativada.");
+        }
+    }
+
+    public String toString(){
+        return String.format("Nome do cliente: %s, Numero da conta: %d, Saldo: %.2f, Ativa: %b.", nomeCliente, numeroConta, saldo, ativa);
+    }
+    public void exibir() {
+        System.out.println(toString());
+    }
 }
--- Principal.java
+++ Principal.java
@@ -1,6 +1,59 @@
 package QuestoesPOO;
 public class Principal {
     public static void main(String[] args) {
+        Conta conta1 = new Conta("Ronaldo", 123);
+        Conta conta2 = new Conta("Berinjela", 456);
+        Conta conta3 = new Conta("Elpepe", 789);
         
+
+        conta1.exibir();
+        System.out.println("-----------------------------");
+        conta2.exibir();
+        System.out.println("-----------------------------");
+        conta3.exibir();
+        System.out.println("-----------------------------");
+
+        conta1.depositar(10);
+        conta2.depositar(20);
+        conta3.depositar(30);
+        System.out.println("-----------------------------");
+        System.out.println("-----------------------------");
+        System.out.println("-----------------------------");
+
+
+
+        conta1.exibir();
+        System.out.println("-----------------------------");
+        conta2.exibir();
+        System.out.println("-----------------------------");
+        conta3.exibir();
+        System.out.println("-----------------------------");
+
+
+        conta1.setNomeCLiente("Renan");
+        conta2.setNomeCLiente("Caveira");
+        conta3.setNomeCLiente("Games");
+
+        conta1.sacar(5);
+        conta2.sacar(10);
+        conta3.sacar(15);
+        System.out.println("-----------------------------");
+        System.out.println("-----------------------------");
+        System.out.println("-----------------------------");
+
+        conta1.exibir();
+        conta2.exibir();
+        conta3.exibir();
+        System.out.println("-----------------------------");
+        System.out.println("-----------------------------");
+        System.out.println("-----------------------------");
+
+        conta1.desativar();
+        conta2.desativar();
+        conta3.desativar();
+
+        conta1.exibir();
+        conta2.exibir();
+        conta3.exibir();
     }
 }
